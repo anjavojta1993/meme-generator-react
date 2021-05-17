9692b2c629a795c1a4d87a29e380dead6aedf881
anjavojta1993 <anja.vojta@gmail.com>
2021-05-17 16:49:34 +0200

    Include comments

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import DownloadButton from './components/DownloadButton';
 import Inputs from './components/Inputs';
 
 function App() {
-  const [image, setImage] = useState('');
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
+  // create App component
+  const [image, setImage] = useState(''); // declare variable for setting the type of character e.g. bender
+  const [topText, setTopText] = useState(''); // declare variable for setting the toptext
+  const [bottomText, setBottomText] = useState(''); // declare variable for setting the bottomtext
   const [customMeme, setCustomMeme] = useState(
-    'https://api.memegen.link/images/saltbae/Let_me/salt_you.png',
+    'https://api.memegen.link/images/saltbae/Let_me/salt_you.png', // declare variable for changing the URL for picture display
   );
-  const [templates, setTemplates] = useState([]);
+  const [templates, setTemplates] = useState([]); // declare empty array to later save objects from templates site in it
 
   const url = 'https://api.memegen.link/templates/';
 
@@ -20,20 +21,22 @@ function App() {
     setCustomMeme(
       `https://api.memegen.link/images/${image}/${topText}/${bottomText}.png`,
     );
-  }
+  } // function to execute on click of "Create meme" button
 
   useEffect(() => {
+    // useEffect Hook is like componentDidMount, componentDidUpdate & componentWillUnmount combined // side effects like fetching data need to be done via UseEffect hook
     const fetchData = async () => {
+      // async await function to fetch data
       try {
-        const response = await fetch(url);
-        const json = await response.json();
-        setTemplates(json);
+        const response = await fetch(url); // await fetch of url => returns a promise
+        const json = await response.json(); // await response (objects of templates url)
+        setTemplates(json); // setTemplates (empty array) to response
       } catch (e) {
         console.error(e);
       }
     };
-    fetchData();
-  }, []);
+    fetchData(); // call the function
+  }, []); // dependency array --> specifies the condition to trigger use effect, if empty array => function will run exactly once, no re-renders, otherwise infinite loop
 
   return (
     <main>
